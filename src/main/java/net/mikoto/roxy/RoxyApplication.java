6cac0c017b497e2883ecc7f551a68b9638790ada
mikoto <mikoto@ungine.cn>
2022-10-14 22:22:01 +0800

    init

---
+++ src/main/java/net/mikoto/roxy/RoxyApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.roxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/roxy/RoxyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.roxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
